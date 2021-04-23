d93015c4ddc095b6405623c5f8087d7e66bc3be3
Othman Shareef <othmanosx@gmail.com>
2021-04-23 21:39:07 +0300

    Feat: create weather card

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Typography, CardContent, Card } from "@material-ui/core"
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+})
+
+export default function OutlinedCard(props) {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        ></Typography>
+        <Typography variant="h5" component="h2" color="secondary">
+          {props.city}, {props.country}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {props.date}
+        </Typography>
+        <Typography variant="h2" component="p">
+          {props.temp}°c
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
